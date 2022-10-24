54191198142250015687d12bcd60e32e47079524
Samandar3464 <107240826+Samandar3464@users.noreply.github.com>
2022-10-24 13:04:32 +0500

    oxshadi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,8 @@
 package org.example;
 
-import org.example.service.PostService;
-
-import java.util.Scanner;
-import java.util.Stack;
-
 public class Main {
     public static void main(String[] args) {
-        Stack<Integer> nums = new Stack<>();
 
+        System.out.println(" Login");
     }
 }
