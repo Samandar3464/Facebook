a2f112a3ab957896ac4e6041dfecd6d7e25f478e
Asrorbek <asrorbekazamjon@gmail.com>
2022-10-24 19:00:45 +0500

    MessageService messages ctructure changed to Hash

--- src/main/java/org/example/service/MessageService.java
+++ src/main/java/org/example/service/MessageService.java
@@ -3,28 +3,24 @@ package org.example.service;
 import org.example.model.Massege;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public  class MessageService {
-    public List<Massege> messages = new ArrayList<>();
+    public Map<Integer,Massege> messages = new HashMap<>();
     public boolean addMessage(Massege massege){
-       return messages.add(massege);
+       messages.put(massege.getId(),massege);
+       return true;
     }
-    public void showMessageByUserId(int userId){
-        for (Massege message : messages) {
-            if (message.getSenderId()==userId||message.getReceiverId()==userId){
-                System.out.println(message.getMassage());
-            }
-        }
-    }
-    public boolean deleteMessageByUserId(int messageId, int userid){
-        for (int i = 0; i < messages.size(); i++) {
-            if(messages.get(i).getId()==messageId&&messages.get(i).getSenderId()==userid){
-                messages.remove(i);
-                return true;
-            }
+    public boolean deleteMessage(int messageId, int userid){
+        if (messages.get(messageId)!=null&&messages.get(messageId).getSenderId()==userid){
+            messages.remove(messageId);
         }
         return false;
+    }
+    public Massege getMessageById(int messageId){
+        return messages.get(messageId);
     }
 
 }
