6c7972f7b846d04a40d2ecfbfb679178cbc6ad31
Asrorbek <79511854+asrorbek07@users.noreply.github.com>
2022-10-25 00:42:48 +0500

    d

---
+++ src/main/java/org/example/service/ChatService.java
@@ -0,0 +1,17 @@
+package org.example.service;
+
+import org.example.model.Chat;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ChatService {
+    public Map<Integer,Chat> chats = new HashMap();
+    protected boolean addNewChat(Chat chat){
+        if (chats.get(chat.getId())!=null) return false;
+        chats.put(chat.getId(),chat);
+        return true;
+    }
+}
