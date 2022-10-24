5e3f0b80661fa18c27c800150925bfa1a1510615
AzmiddinAbdullayev <abdullayevazmiddin@gmail.com>
2022-10-24 14:28:15 +0500

    last

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println(true);
         System.out.println("GIT");
         System.out.println("Hello Git");
+        System.out.println("last check");
     }
 }
