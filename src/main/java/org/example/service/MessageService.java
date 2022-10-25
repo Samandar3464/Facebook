3e9508e8338dda69eb6349a7b7c9125a8d6b18bf
asrorbek07 <asrorobidjonov2000@gmail.com>
2022-10-25 13:09:21 +0500

    main pulled

--- src/main/java/org/example/service/MessageService.java
+++ src/main/java/org/example/service/MessageService.java
@@ -1,26 +1,31 @@
 package org.example.service;
 
+import org.example.DataBase;
 import org.example.model.Massege;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public  class MessageService {
-    public Map<Integer,Massege> messages = new HashMap<>();
-    public boolean addMessage(Massege massege){
-       messages.put(massege.getId(),massege);
+
+    public boolean addMessage(Massege massege) throws IOException {
+
+
        return true;
     }
-    public boolean deleteMessage(int messageId, int userid){
+    public boolean deleteMessage(int messageId, int userid) throws IOException {
+               HashMap<Integer,Massege> messages = DataBase.massageRead();
         if (messages.get(messageId)!=null&&messages.get(messageId).getSenderId()==userid){
             messages.remove(messageId);
+            DataBase.massageWrite(messages);
         }
         return false;
     }
-    public Massege getMessageById(int messageId){
-        return messages.get(messageId);
-    }
+//    public Massege getMessageById(int messageId){
+//
+//    }
 
 }
