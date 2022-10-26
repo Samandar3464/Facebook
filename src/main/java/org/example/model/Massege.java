e4a3148df7a9f3eba5ecd687e5c063178c341fa7
asrorbek07 <asrorobidjonov2000@gmail.com>
2022-10-26 12:36:23 +0500

    pulled from Samander

--- src/main/java/org/example/model/Massege.java
+++ src/main/java/org/example/model/Massege.java
@@ -9,6 +9,6 @@ public class Massege extends Base {
     private  int receiverId;
     private  String senderName;
     private String massage;
-    private  String senderName;
+    private  String receiverName;
 
 }
