b048b22405f62ccb9c62f6787daf3b562cf35f14
Samandar3464 <107240826+Samandar3464@users.noreply.github.com>
2022-10-26 16:34:28 +0500

    added

--- src/main/java/org/example/model/Commit.java
+++ src/main/java/org/example/model/Commit.java
@@ -3,9 +3,17 @@ package org.example.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.Base;
+
+import java.security.Signature;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 @Data
-@NoArgsConstructor
 public class Commit extends Base {
     private String commit;
     private int postId;
+
+    public Commit() {
+    }
 }
--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -3,14 +3,17 @@ package org.example.service;
 import org.example.DataBase;
 import org.example.model.User;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
 public class UserService {
 
     public boolean registration(User user) {
-       // DataBase.allUsers=new ArrayList<>();
+
             for (User allUser : DataBase.allUsers) {
                 if (allUser != null) {
                     if (allUser.getPhoneNumber().equals(user.getPhoneNumber())) {
-                       // DataBase.allUsers.add(user);
+
                         return false;
                     }
                 }
@@ -36,7 +39,6 @@ public class UserService {
             if (allUser!=null){
                 if (allUser.getId()==userId){
                     return getById(userId);
-
                 }
             }
         }
@@ -62,4 +64,15 @@ public class UserService {
         }
         return null;
     }
+
+    public  void showFriends(User user){
+        for (User allUser : DataBase.allUsers) {
+            if (allUser!=null){
+                if (allUser.equals(user)){
+                    System.out.println(allUser.getFriendsId());
+                }
+            }
+        }
+    }
+
 }
