bcbf133966ef0fb9965335eb12d9f539191b3343
asrorbek07 <asrorobidjonov2000@gmail.com>
2022-10-27 16:38:19 +0500

    commit

--- src/main/java/org/example/dto/UserDto.java
+++ src/main/java/org/example/dto/UserDto.java
@@ -106,41 +106,4 @@ public class UserDto {
         return userService.logIn(phone, password);
     }
 
-    public  void chatFront(User user){
-        int var=10;
-        while (var!=0){
-            chatService.showChats(user.getChatId(), user.getId());
-            System.out.println("\n"+" 1.Add chat 2.Chat with one contact 3. Delete chat");
-           var=scannerInt.nextInt();
-            switch (var){
-            case 1->{
-                userService.showFriends(user);
-
-            }
-            case 2->{
-                System.out.println();
-              //  chatService.getChatById(user.getChatId())
-            }
-            case 3->{}
-            case 0->{}
-            }
-        }
-
-    }
-    public  boolean add(User user) throws IOException {
-       return userService.registration(user);
-    }
-
-    public static boolean logIn() throws IOException {
-        System.out.println("Enter phoneNumber or your gmail: ");
-        String phoneNumber=scannerStr.nextLine();
-        System.out.println("Enter PASSWORD: ");
-        String password=scannerStr.nextLine();
-
-        User login = userService.logIn(phoneNumber,password);
-        if (login!=null){
-            return true;
-        }
-        return false;
-    }
 }
--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -4,70 +4,36 @@ import org.example.DataBase;
 import org.example.dto.UserDto;
 import org.example.model.User;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
-
 
 public class UserService {
-
-    int number = 0;
-
-
-    public void sendSms(String phoneNumber) {
-
-        number = (int) (Math.random() * (9999 - 999 - 1) + 999);
-        System.out.println("SmS sent to +998(**)***" + phoneNumber.substring(5) + " number! " + number);
-    }
-
-    public boolean receiveTheCode(int code) {
-        if (code == number) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean edit(int id) throws IOException {
-        User user = (User) getById(id);
-        if (user != null) {
-            if (user.getId() == id) {
-                UserDto.logIn();
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean registration(User user) throws IOException {
-        for (User allUser : DataBase.allUsers) {
-            if (login(user.getPhoneNumber()) == null) {
-                return false;
-            }
-        }
-        DataBase.allUsers.add(user);
-        return true;
-    }
-
-    private User login(String phoneNumber) throws IOException {
-        if (DataBase.allUsers != null) {
-            for (User user : DataBase.allUsers) {
-                if (user != null) {
-                    if (user.getPhoneNumber().equals(phoneNumber)) {
-                        return user;
+    public boolean registration(User user) {
+            for (User allUser : DataBase.allUsers) {
+                if (allUser != null&&allUser.isActive()) {
+                    if (allUser.getPhoneNumber().equals(user.getPhoneNumber())||allUser.getUserName().equals(user.getUserName())) {
+                        return false;
                     }
                 }
             }
+        DataBase.allUsers.add(user);
+        return true;
+    }
+  
+    public User logIn(String phoneNumber, String password) {
+        if (DataBase.allUsers == null) return null;
+        for (User user : DataBase.allUsers) {
+            if (user !=null&& user.isActive()){
+                if (user.isActive()&&user.getPhoneNumber().equals(phoneNumber)&& user.getPassword().equals(password)){
+                    return user;
+                }
+            }
         }
-
         return null;
     }
-
-    public User login(String phoneNumber, String password) throws IOException {
-
+    public User getById(int userId) {
         for (User user : DataBase.allUsers) {
-            if (user != null) {
-                if (user.getPhoneNumber().equals(phoneNumber) && user.getPassword().equals(password)) {
+            if (user != null&&user.isActive()) {
+                if (user.getId() == userId) {
                     return user;
                 }
             }
@@ -75,26 +41,11 @@ public class UserService {
         return null;
     }
 
-    public int getIdByPhoneNumber(String phoneNumber) {
-        for (User user : DataBase.allUsers) {
-            if (user != null) {
-                if (user.getPhoneNumber().equals(phoneNumber)) {
-                    return user.getId();
-                }
-            }
-        }
-        return 0;
-    }
-
-
-    public User getById(int userId) throws IOException {
+    public User getByUserName(String userName) {
         for (User allUser : DataBase.allUsers) {
-            if (allUser != null) {
-                if (allUser.getId() == userId) {
-                    return allUser;
-                }
-            }
+            if (allUser!=null&&allUser.isActive()&&allUser.getUserName().equals(userName)) return allUser;
         }
         return null;
     }
 }
+
